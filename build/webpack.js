534092359cece88e3dffd040f25d784d850d0f64
ovo4096 <ovo4096@gmail.com>
2017-11-30 15:35:52 +0800

    添加 webpack 配置

--- build/webpack.js
+++ build/webpack.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: path.resolve(__dirname, '..', 'src/index.js'),
     output: {
         path: path.resolve(__dirname, '..', 'dist'),
         filename: '[hash].bundle.js'
-    }
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist'], {
+            root: path.resolve(__dirname, '..')
+        }),
+        // new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
 };
